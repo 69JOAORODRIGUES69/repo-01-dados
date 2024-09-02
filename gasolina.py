0c176ea234da86f0b3a74c514c3a762d0efa8317
69JOAORODRIGUES69 <vbrodrigues652@gmail.com>
2024-09-02 18:05:31 -0300

    Update gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Carregar os dados
+df = pd.read_csv('gasolina.csv')
+
+# Gerar o gráfico
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x='dia', y='venda')
+plt.title('Preço da Gasolina em São Paulo - Julho 2021')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+plt.savefig('gasolina.png')
+plt.show()
 
